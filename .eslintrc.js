72519286fe63388323db4b82738431740f4d2803
Slava Vorobev <my_post_office1@mail.ru>
2023-05-04 13:46:55 +0300

    feat/#58: add buildSlice

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
         'ulbi-tv-plugin',
     ],
     rules: {
+        semi: 0,
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
@@ -82,4 +83,4 @@ module.exports = {
             },
         },
     ],
-};
+}
--- src/d-widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/d-widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,35 +1,38 @@
-import { FC, memo, useState } from 'react';
-import { ThemeSwitcher } from '../../../ThemeSwitcher';
-import { SidebarItemsList } from '../../model/items';
-import { SidebarItem } from '../SidebarItem/SidebarItem';
-import './Sidebar.scss';
-import { Theme, useTheme } from '@/app/providers/ThemeProvider';
-import { classNames as cn } from '@/g-shared/lib/classNames';
+import './Sidebar.scss'
+import { FC, memo, useState } from 'react'
+import { ThemeSwitcher } from '../../../ThemeSwitcher'
+import { SidebarItemsList } from '../../model/items'
+import { SidebarItem } from '../SidebarItem/SidebarItem'
+import { Theme, useTheme } from '@/app/providers/ThemeProvider'
+import { classNames as cn } from '@/g-shared/lib/classNames'
+import { Mods } from '@/g-shared/lib/classNames/classNames'
 import {
     Button, Column, Menu, Row,
-} from '@/g-shared/ui';
-import { BtnSize, BtnVariant } from '@/g-shared/ui/Button/types';
-import { Mods } from '@/g-shared/lib/classNames/classNames';
+} from '@/g-shared/ui'
+import { BtnSize, BtnVariant } from '@/g-shared/ui/Button/types'
 
 interface SidebarProps {
-  className?: string
+    className?: string
 }
 
 export const Sidebar: FC<SidebarProps> = memo((props) => {
-    const { className } = props;
+    const { className } = props
 
-    const { theme } = useTheme();
+    const { theme } = useTheme()
 
-    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(false)
 
     const mods: Mods = {
         'sidebar--collapsed': isCollapsed,
         'sidebar--light': theme === Theme.LIGHT,
         'sidebar--dark': theme === Theme.DARK,
-    };
+    }
 
     return (
-        <aside className={cn('sidebar', mods, [className])} data-testid="sidebar">
+        <aside
+            className={cn('sidebar', mods, [className])}
+            data-testid="sidebar"
+        >
             <Row direction="column" justify="space-between" isFullHeight>
                 <Column isFlexNone>
                     <Menu direction="column" rowGap="level1">
@@ -57,5 +60,5 @@ export const Sidebar: FC<SidebarProps> = memo((props) => {
                 </Column>
             </Row>
         </aside>
-    );
-});
+    )
+})
